c1750d7ed7b9cd04929a9c2002ba722283f0b4cc
kairas83 <kairas83@live.co.kr>
2016-04-01 21:52:03 +0900

    change plan.equals("gold") to "gold".equals(plan)

--- src/main/java/org/iptime/kairas/phonebill/App.java
+++ src/main/java/org/iptime/kairas/phonebill/App.java
@@ -10,21 +10,21 @@ public class App
     {
     	AppView test = new AppView();
     	
-    	System.out.print("요금제를 입력하세요(gold, silver)");
+    	test.outputString("요금제를 입력하세요(gold, silver)");
     	String plan = test.inputString();
     	
-    	System.out.print("총 사용 시간(분)을 입력하세요:");
+    	test.outputString("총 사용 시간(분)을 입력하세요:");
     	int time = test.inputInt();
     	
-    	System.out.print("총 사용 회선을 입력하세요:");
+    	test.outputString("총 사용 회선을 입력하세요:");
     	int line = test.inputInt();
     	
     	Calculator cal = new Calculator();
     	double bill = 0;
     	
-    	if (plan.equals("gold")) 
+    	if ("gold".equals(plan)) 
     		bill = cal.goldCost(time, line);
-    	else if (plan.equals("silver"))
+    	else if ("silver".equals(plan))
     		bill = cal.silverCost(time, line);
     	
     	test.outputString(""+bill);
