144eeb3c05631e61b17620de9e62e4acc5a4196d
sanggihong <sanggi.hong@gmail.com>
2016-04-02 20:06:43 +0900

    implements phonebill class

--- src/main/java/org/iptime/kairas/phonebill/PhoneBill.java
+++ src/main/java/org/iptime/kairas/phonebill/PhoneBill.java
@@ -1,39 +1,32 @@
 package org.iptime.kairas.phonebill;
 
 public class PhoneBill {
+	private Calculator calc;
+	private Account account;
+	private StringBuilder bill;
+	
+	private PhoneBill(){
+		//hide empty constructor
+	}
+	
+	public PhoneBill(Account aAccount){
+		calc = new Calculator();
+		account = aAccount;
+		calc.run(account);
+	}
+	
 	public void setAccount(Account aAccount){
-		
+		account = aAccount;
+		calc.run(account);
 	}
 	
-	public void addPlan(Plan aPlan){
-		
-	}
-	
-	public double getTotalCost(){
-		
-	}
-	
-	public String getFomulaOfCost(){
-		
-	}
-	
-	public String getPlanTypeOfAccount(){
-		
-	}
-	
-	public String getBasicMonthlyRate(){
-		
-	}
-	
-	public String getRatePerExcessMinute(){
-		
-	}
-	
-	public getIncludedMinutes(){
-		
-	}
-	
-	public getAdditionalOptions(){
-		
+	public StringBuilder getBill(){
+		bill = new StringBuilder();
+		//make string like table about bill with StringBuilder
+		/*--------------------------------------------
+		 * plan|munutesUsed|numberOflines|ExpectedBill
+		 *--------------------------------------------
+		 *blahblah...
+		 */
 	}
 }
